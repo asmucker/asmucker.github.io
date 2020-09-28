3d0508b275a5cc5eef2d981be1af6b31b8c18fc8
asmucker <69869496+asmucker@users.noreply.github.com>
2020-09-27 21:23:00 -0400

    Updated
    Changed name and deleted old copy

--- project-one/assets/toggle.js
+++ project-one/assets/toggle.js
@@ -2,6 +2,6 @@ $(document).ready(function (){
     //ID or Class name of the button goes into the first section
     $('#toggle_button').click(function() {
         //ID or Class name of the modal you want to appear on click goes into this second area.
-        $("#letter_analysis").toggleClass("visible");
+        $("#letter-analysis").toggleClass("visible");
         });
 });
